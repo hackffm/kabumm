f7fd615ac067076f6e5a29bc29e8aeb0154f33d3
kabumm raspi <invalid@example.com>
2025-09-24 21:30:55 +0200

    Fix initial commit

--- python/src/kabumm/backend.py
+++ python/src/kabumm/backend.py
@@ -3,41 +3,44 @@ import contextlib
 
 import board
 import neopixel
-import gpoid
+import gpiod
 from adafruit_ht16k33.segments import Seg14x4
 
 logger = logging.getLogger(__name__)
 
 class RaspiBackend:
-    def __init__(self, gpio_request, num_wires_and_pixels: int, port_number_of_pixel: list[int]):
+    def __init__(self, gpio_request, num_wires_and_pixels: int, port_number_of_pixels: list[int]):
         logger.debug("creating display...")
         self.i2c = board.I2C()
         self.display = Seg14x4(self.i2c, address=0x70)
         logger.debug("created display")
 
         logger.debug("creating pixels...")
-        self.pixels = neopixel.NeoPixel(board.D18, num_wires_and_pixels, pixel_order=GRB, auto_write=False)
+        self.pixels = neopixel.NeoPixel(board.D18, num_wires_and_pixels, pixel_order=neopixel.GRB, auto_write=False)
         self.pixels.fill((0,0,0))
+        self.pixels.show()
         logger.debug("created pixels")
 
         self.gpio_request = gpio_request
+        self.port_number_of_pixels = port_number_of_pixels
 
 
-    def write(string):
+    def write(self, string):
         logger.info("display %s", string)
         self.display.print(string)
 
-    def set_pixels(values):
+    def set_pixels(self, values):
         logger.info("set pixels to %s", values)
         self.pixels[:] = values
+        self.pixels.show()
 
-    def is_cut() -> list[bool]:
-        gpio_lines = self.gpio_request.get_values(self.port_number_of_pixel)
-        values = [gpio_lines[port] == gpiod.line.Value.ACTIVE for port in self.port_number_of_pixel]
-        logger.debug("read wires: %s", "".join(hex(i)[-1] if values[i] else "_" for i in range(len(self.port_number_of_pixel))))
+    def is_cut(self) -> list[bool]:
+        gpio_lines = self.gpio_request.get_values(self.port_number_of_pixels)
+        values = [gpio_lines[port] == gpiod.line.Value.ACTIVE for port in self.port_number_of_pixels]
+        logger.debug("read wires: %s", "".join(hex(i)[-1] if values[i] else "_" for i in range(len(self.port_number_of_pixels))))
         return values
 
-def _get_chip_path(self) -> str:
+def _get_chip_path() -> str:
     candidates = []
     for i in range(10):
         path = f"/dev/gpiochip{i}"
@@ -62,17 +65,17 @@ def _get_chip_path(self) -> str:
 def make_backend(num_wires_and_pixels: int, port_number_of_pixel: list[int]|None=None):
     if not port_number_of_pixel:
         port_number_of_pixel = list(range(num_wires_and_pixels))
-    assert len(self.port_number_of_pixel) == num_wires_and_pixels
-    assert len(set(self.port_number_of_pixel)) == num_wires_and_pixels, "port_number_of_pixel values must be unique"
-    assert all((v <16 for v in self.port_number_of_pixel)), "port numbers must be <16"
+    assert len(port_number_of_pixel) == num_wires_and_pixels
+    assert len(set(port_number_of_pixel)) == num_wires_and_pixels, "port_number_of_pixel values must be unique"
+    assert all((v <16 for v in port_number_of_pixel)), "port numbers must be <16"
 
     logger.debug("creating gpio expander chip...")
-    with gpiod.Chip(path=self._get_chip_path()) as chip:
+    with gpiod.Chip(path=_get_chip_path()) as chip:
         logger.debug("created gpio expander chip")
         settings = gpiod.LineSettings(direction=gpiod.line.Direction.INPUT, bias=gpiod.line.Bias.PULL_UP)
-        request = selfchip.request_lines({port: settings for port in port_number_of_pixel})
+        request = chip.request_lines({port: settings for port in port_number_of_pixel})
         try:
-            yield RaspiBackend(num_wires_and_pixels, request, port_number_of_pixel)
+            yield RaspiBackend(request, num_wires_and_pixels, port_number_of_pixel)
         finally:
             request.release()
     logger.info("backend cleaned up during normal exit")
